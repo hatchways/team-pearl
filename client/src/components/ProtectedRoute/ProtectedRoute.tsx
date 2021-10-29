ebf5cbc229ceb192bca615a627a159d022e8e63b
amisdun <michealdunamis@gmail.com>
2021-10-29 15:36:07 +0000

    amisdun: correction made

--- client/src/components/ProtectedRoute/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -8,27 +8,9 @@ interface ProtectedRouteProps extends Omit<RouteProps, 'component'> {
 
 export const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ component: Component, ...rest }) => {
   const { loggedInUser } = useAuth();
-  return (
-    <Route
-      {...rest}
-      render={(props) => {
-        if (loggedInUser && Object.keys(loggedInUser).length) {
-          return <Component {...rest} {...props} loggedInUser={loggedInUser} />;
-        } else if (loggedInUser === undefined) {
-          return <CircularProgress />;
-        } else {
-          return (
-            <Redirect
-              to={{
-                pathname: '/login',
-                state: {
-                  from: props.location,
-                },
-              }}
-            />
-          );
-        }
-      }}
-    />
+  return loggedInUser ? (
+    <Route {...rest} render={(props) => <Component {...rest} {...props} />} />
+  ) : (
+    <Redirect to="/login" />
   );
 };
