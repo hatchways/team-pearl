df1b13f4781cd246b5e9fd955e65ce8de0d94788
amisdun <michealdunamis@gmail.com>
2021-10-29 15:37:20 +0000

    amisdun: minor fixes

--- client/src/components/ProtectedRoute/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -7,25 +7,9 @@ interface ProtectedRouteProps extends Omit<RouteProps, 'component'> {
 
 export const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ component: Component, ...rest }) => {
   const { loggedInUser } = useAuth();
-  return (
-    <Route
-      {...rest}
-      render={(props) => {
-        if (loggedInUser) {
-          return <Component {...rest} {...props} />;
-        } else {
-          return (
-            <Redirect
-              to={{
-                pathname: '/login',
-                state: {
-                  from: props.location,
-                },
-              }}
-            />
-          );
-        }
-      }}
-    />
+  return loggedInUser ? (
+    <Route {...rest} render={(props) => <Component {...rest} {...props} />} />
+  ) : (
+    <Redirect to="/login" />
   );
 };
